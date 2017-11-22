d8f84c306ab0da0fde8c89ad0040e641bb0574c1
Gary Hall <guategary@gmail.com>
2017-11-21 22:29:16 -0500

    Add session controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,26 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+
+    user = User.find_by(name: session_params[:name])
+
+    if user
+      cookies.permanent[:user] = user.id
+
+      @user = User.find(user.id)
+
+      render 'user/show'
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+
+    def session_params
+      params.require(:session).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
+  get root to: 'session#new'
+
   resources :user, only: [:new, :create, :show]
+  resources :session, only: [:new, :create]
 end
